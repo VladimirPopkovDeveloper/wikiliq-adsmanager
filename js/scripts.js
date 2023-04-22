fa7f85f72840488e2d688cc5d1948aaba546aa5d
Volodimir Popkov <dj-vell@ya.ru>
2023-04-22 19:37:48 +0400

    Add notifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Navbar
 document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(
@@ -5,34 +6,42 @@ document.addEventListener("DOMContentLoaded", () => {
     0
   );
 
-  // Add a click event on each of them
   $navbarBurgers.forEach((el) => {
     el.addEventListener("click", () => {
-      // Get the target from the "data-target" attribute
       const target = el.dataset.target;
       const $target = document.getElementById(target);
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       el.classList.toggle("is-active");
       $target.classList.toggle("is-active");
     });
   });
 });
-// Получаем все вкладки и контейнеры
+
+// Tabs & tab content
 const tabs = document.querySelectorAll(".tabs li");
 const tabContentBoxes = document.querySelectorAll(
   "#tab-content .tab-content-box"
 );
 
-// Добавляем обработчик событий для каждой вкладки
 tabs.forEach((tab, index) => {
   tab.addEventListener("click", () => {
-    // Удаляем класс is-active со всех вкладок и контейнеров
     tabs.forEach((tab) => tab.classList.remove("is-active"));
     tabContentBoxes.forEach((box) => box.classList.remove("is-active"));
 
-    // Добавляем класс is-active только для нажатой вкладки
     tab.classList.add("is-active");
     tabContentBoxes[index].classList.add("is-active");
   });
+});
+
+// Notifications
+document.addEventListener("DOMContentLoaded", () => {
+  (document.querySelectorAll(".notification .delete") || []).forEach(
+    ($delete) => {
+      const $notification = $delete.parentNode;
+
+      $delete.addEventListener("click", () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    }
+  );
 });
