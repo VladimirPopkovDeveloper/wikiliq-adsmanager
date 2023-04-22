5cc92cc13ce417ffcd0ff20321ff309f0109a584
Volodimir Popkov <dj-vell@ya.ru>
2023-04-22 23:10:17 +0400

    Add modals Fix some mobile styles

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
       const target = el.dataset.target;
       const $target = document.getElementById(target);
 
-      el.classList.toggle("is-active");
       $target.classList.toggle("is-active");
     });
   });
