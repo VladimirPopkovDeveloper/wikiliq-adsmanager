bd56f79cded0c6202f03f86d568889100c0e99fe
Volodimir Popkov <dj-vell@ya.ru>
2023-04-27 19:00:24 +0400

    Change folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+// Определяем переменную "preprocessor"
+let preprocessor = "sass"; // Выбор препроцессора в проекте - sass или less
+
 // Определяем константы Gulp
 const { src, dest, parallel, series, watch } = require("gulp");
 
@@ -10,6 +13,16 @@ const concat = require("gulp-concat");
 // Подключаем gulp-uglify-es
 const uglify = require("gulp-uglify-es").default;
 
+// Подключаем модули gulp-sass и gulp-less
+const sass = require("gulp-sass")(require("sass"));
+const less = require("gulp-less");
+
+// Подключаем Autoprefixer
+const autoprefixer = require("gulp-autoprefixer");
+
+// Подключаем модуль gulp-clean-css
+const cleancss = require("gulp-clean-css");
+
 // Определяем логику работы Browsersync
 function browsersync() {
   browserSync.init({
@@ -20,11 +33,13 @@ function browsersync() {
   });
 }
 
+// Обрабатываем скрипты
 function scripts() {
   return src([
     // Берем файлы из источников
     "node_modules/alpinejs/dist/cdn.min.js", // Alpine JS
-    "src/js/modal-fx.js", // Bulma Modal FX
+    "src/js/modal-fx.js", // Modified Bulma Modal FX
+    "src/js/scripts.js", // Project custom JS
   ])
     .pipe(concat("scripts.min.js")) // Конкатенируем в один файл
     .pipe(uglify()) // Сжимаем JavaScript
@@ -32,8 +47,36 @@ function scripts() {
     .pipe(browserSync.stream()); // Триггерим Browsersync для обновления страницы
 }
 
+// Обрабатываем стили
+function styles() {
+  return src("src/" + preprocessor + "/styles." + preprocessor + "") // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+    .pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
+    .pipe(concat("styles.min.css")) // Конкатенируем в файл app.min.js
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ["last 10 versions"], grid: true })
+    ) // Создадим префиксы с помощью Autoprefixer
+    .pipe(
+      cleancss({
+        level: { 1: { specialComments: 0 } } /* , format: 'beautify' */,
+      })
+    ) // Минифицируем стили
+    .pipe(dest("css/")) // Выгрузим результат в папку "app/css/"
+    .pipe(browserSync.stream()); // Сделаем инъекцию в браузер
+}
+
+function startwatch() {
+  // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
+  watch(["src/**/*.js", "!src/**/*.min.js"], scripts);
+}
+
 // Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
 exports.browsersync = browsersync;
 
 // Экспортируем функцию scripts() в таск scripts
 exports.scripts = scripts;
+
+// Экспортируем функцию styles() в таск styles
+exports.styles = styles;
+
+// Экспортируем дефолтный таск с нужным набором функций
+exports.default = parallel(scripts, browsersync, startwatch);
---
+++ src/js/scripts.js
@@ -0,0 +1,50 @@
+// Navbar
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  $navbarBurgers.forEach((el) => {
+    el.addEventListener("click", () => {
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      $target.classList.toggle("is-active");
+    });
+  });
+});
+
+// Tabs & tab content
+const tabs = document.querySelectorAll(".tabs li");
+const tabContentBoxes = document.querySelectorAll(
+  "#tab-content .tab-content-box"
+);
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => tab.classList.remove("is-active"));
+    tabContentBoxes.forEach((box) => box.classList.remove("is-active"));
+
+    tab.classList.add("is-active");
+    tabContentBoxes[index].classList.add("is-active");
+  });
+});
+
+// Notifications
+document.addEventListener("DOMContentLoaded", () => {
+  (document.querySelectorAll(".notification .delete") || []).forEach(
+    ($delete) => {
+      const $notification = $delete.parentNode;
+
+      $delete.addEventListener("click", () => {
+        $notification.classList.remove("animate__slideInUp");
+        $notification.classList.add("animate__slideOutDown");
+        setTimeout(() => {
+          $notification.parentNode.removeChild($notification);
+        }, 1000);
+      });
+    }
+  );
+});
